7729f616b1dad88348ea7799d803fc41473e1f25
Mikey Badr <mfbadr@gmail.com>
2014-07-22 11:55:27 -0500

    .high and .low

--- app/models/weather.js
+++ app/models/weather.js
@@ -11,7 +11,6 @@ Weather.high = function(zip, cb){
   var url = 'http://api.wunderground.com/api/2738df9035fbffa9/forecast/q/' + zip + '.json';
   request( url, function(error,response,body){
     body = JSON.parse(body);
-  
     cb(body.forecast.simpleforecast.forecastday[0].high.fahrenheit + ' F');
   });
 };
@@ -21,7 +20,6 @@ Weather.low = function(zip, cb){
   var url = 'http://api.wunderground.com/api/2738df9035fbffa9/forecast/q/' + zip + '.json';
   request( url, function(error,response,body){
     body = JSON.parse(body);
-
     cb(body.forecast.simpleforecast.forecastday[0].low.fahrenheit + ' F');
  });
 };
